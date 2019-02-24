d683bba32ca9522ce70df1c8c77219cd8e5bdf3e
Esteban Ramos Fernández <esteban.ramos.f@gmail.com>
2019-02-23 22:50:25 -0500

    update - se agregan los controladores para todos los modelos que existen

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+/*
+Route::resource('/frontend/cargos', '');
+Route::resource('/frontend/contratos', '');
+Route::resource('/frontend/cuentas', '');
+Route::resource('/frontend/establecimientos', '');
+Route::resource('/frontend/fichas_trabajadores', '');
+Route::resource('/frontend/ingresos', '');
+Route::resource('/frontend/libros', '');
+Route::resource('/frontend/libros_cuentas', '');
+Route::resource('/frontend/permisos', '');
+Route::resource('/frontend/roles', '');
+Route::resource('/frontend/subvenciones', '');
+Route::resource('/frontend/tipos_contratos', '');
+Route::resource('/frontend/tipos_documentos', '');
+Route::resource('/frontend/usuarios', '');
+*/
+
+
+
+
