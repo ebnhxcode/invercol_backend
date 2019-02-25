13dee3c8343962936920f1415f20c9f029af290e
Esteban Ramos Fernández <esteban.ramos.f@gmail.com>
2019-02-25 16:26:04 -0500

    update - respaldo de los dumps

---
+++ database/dumps/unitary/libros.sql
@@ -0,0 +1,4 @@
+insert into libros (libro_nombre,libro_descripcion,libro_codigo) values ('Libro de Compras y Otros Gastos','Sin descripción','Código sin ingresar');
+insert into libros (libro_nombre,libro_descripcion,libro_codigo) values ('Libro de Compras y Honorarios','Sin descripción','Código sin ingresar');
+insert into libros (libro_nombre,libro_descripcion,libro_codigo) values ('Libro de Remuneraciones','Sin descripción','Código sin ingresar');
+insert into libros (libro_nombre,libro_descripcion,libro_codigo) values ('Libro de Otros Ingresos','Sin descripción','Código sin ingresar'
